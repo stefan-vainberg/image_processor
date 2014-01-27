6330e881a49bd83758bb05eee24f068444bdd7c9
Stefan Vainberg <stefan.vainberg@gmail.com>
2014-01-26 16:28:40 -0800

    DeviceImagesFetcher commit

---
+++ DeviceImagesFetcher.h
@@ -0,0 +1,13 @@
+//
+//  DeviceImagesFetcher.h
+//  imageProcessor
+//
+//  Created by Stefan Vainberg on 1/26/14.
+//  Copyright (c) 2014 Stefan Vainberg. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DeviceImagesFetcher : NSObject
+
+@end
---
+++ DeviceImagesFetcher.m
@@ -0,0 +1,13 @@
+//
+//  DeviceImagesFetcher.m
+//  imageProcessor
+//
+//  Created by Stefan Vainberg on 1/26/14.
+//  Copyright (c) 2014 Stefan Vainberg. All rights reserved.
+//
+
+#import "DeviceImagesFetcher.h"
+
+@implementation DeviceImagesFetcher
+
+@end
